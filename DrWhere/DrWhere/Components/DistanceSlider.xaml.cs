ff563e60f9ae06022d178232afe9004b1755a718
lewis dixon <lewis.dixon@live.co.uk>
2019-10-31 23:10:57 +0000

    Begin work on UI

---
+++ DrWhere/DrWhere/Components/DistanceSlider.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DrWhere.Components
+{
+    /// <summary>
+    /// Interaction logic for DistanceSlider.xaml
+    /// </summary>
+    public partial class DistanceSlider : UserControl
+    {
+        public DistanceSlider()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+        }
+
+        public double distanceSelected { get; set; }
+        public Boolean distanceType { get; set; }
+
+        private void DistanceSliderPart_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            this.distanceSelected = this.DistanceSliderPart.Value;
+            this.DistanceLabel.Content = this.distanceSelected.ToString() + (distanceType ? " Km" : " Mi");
+        }
+    }
+}
